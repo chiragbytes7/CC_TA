a817b26db1f065f730a9aa6495ea54b753b3861d
Chirag Agarwal <142739814+chiragbytes7@users.noreply.github.com>
2025-03-11 16:44:31 +0530

    Update chirag.cpp

--- chirag.cpp
+++ chirag.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I PES1UG22CS161, have successfully built this twice and run this on god\n";
+  cout << "I PES1UG22CS161, have successfully built this 2 times and run this on god\n";
   return 0;
 }
