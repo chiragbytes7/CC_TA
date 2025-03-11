daeffc38bf78174ef6bda439253ebb9f376e8dfc
Chirag Agarwal <142739814+chiragbytes7@users.noreply.github.com>
2025-03-11 16:36:09 +0530

    Create chirag.cpp

---
+++ chirag.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I PES1UG22CS161, have successfully built this twice and run this on god\n";
+  return 0;
+}
